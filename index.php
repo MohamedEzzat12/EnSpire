1789a1ce7be2c4bac1b28223ed09d4d48448cd84
MohamedEzzat12 <72480727+MohamedEzzat12@users.noreply.github.com>
2022-08-11 23:33:09 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+if (isset($_POST['sendButton'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    $to = 'EnSpireBLC@gmail.com';
+}
